1547b5820fbccd8cba7232c4aadb6f86f8627b18
Theo22100 <theoguerinBZH@hotmail.com>
2024-09-19 09:33:13 +0200

    add login

--- back/src/configapi.js
+++ back/src/configapi.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
     apiKey: process.env.GOOGLE_API_KEY,
+    jwtSecret: process.env.JWT_SECRET,
 };
 
 module.exports = config;
--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -1,11 +1,27 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../configapi');
+const { Op } = require('sequelize');
 
 // Enregistrer un nouvel utilisateur
 exports.registerUser = async (req, res) => {
     const { pseudo, password, name, firstname, mail, gender, age } = req.body;
 
     try {
+        // pseudo verif
+        const existingUserByPseudo = await User.findOne({ where: { pseudo } });
+        if (existingUserByPseudo) {
+            return res.status(400).json({ error: 'Le pseudo est déjà utilisé.' });
+        }
+
+        // mail verif
+        const existingUserByEmail = await User.findOne({ where: { mail } });
+        if (existingUserByEmail) {
+            return res.status(400).json({ error: 'L\'email est déjà utilisé.' });
+        }
+
+        // Hachage mdp
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             pseudo,
@@ -18,15 +34,53 @@ exports.registerUser = async (req, res) => {
         });
         res.status(201).json({ id: user.id, pseudo: user.pseudo, name: user.name, firstname: user.firstname, mail: user.mail, gender: user.gender });
     } catch (error) {
-        res.status(500).json({ error: 'Erreur création de l\'utilisateur' });
+        res.status(500).json({ error: 'Erreur lors de la création de l\'utilisateur' });
+    }
+};
+
+// Connexion d'un utilisateur
+exports.loginUser = async (req, res) => {
+    const { pseudoOrEmail, password } = req.body;
+
+    try {
+        const user = await User.findOne({
+            where: {
+                [Op.or]: [
+                    { pseudo: pseudoOrEmail },
+                    { mail: pseudoOrEmail }
+                ]
+            }
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: 'Utilisateur non trouvé.' });
+        }
+
+        // mdp
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Mot de passe incorrect.' });
+        }
+
+        // jeton jwt
+        const token = jwt.sign(
+            { id: user.id, pseudo: user.pseudo, mail: user.mail },
+            config.jwtSecret, 
+            { expiresIn: '1h' } 
+        );
+
+
+        res.status(200).json({ token }); 
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la connexion de l\'utilisateur' });
     }
 };
 
 // Récupérer tous les utilisateurs
 exports.getAllUsers = async (req, res) => {
     try {
-        const users = await User.findAll(); // Récupérer tous les utilisateurs
-        res.status(200).json(users); // Retourner la liste des utilisateurs
+        const users = await User.findAll(); 
+        res.status(200).json(users); 
     } catch (error) {
         res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs' });
     }
