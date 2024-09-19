0763a84ac8f7bd90a0ba491b130eae7a9d13a652
Theo22100 <theoguerinBZH@hotmail.com>
2024-09-19 16:10:36 +0200

    add average

--- back/src/controllers/viewController.js
+++ back/src/controllers/viewController.js
@@ -39,3 +39,27 @@ exports.getViewsByIdGoogle = async (req, res) => {
         res.status(500).json({ error: 'Erreur lors de la récupération des avis' });
     }
 };
+
+
+// Récupérer les avis et calculer la moyenne des notes via id_google
+exports.getAverageNoteByIdGoogle = async (req, res) => {
+    const { id_google } = req.params;
+
+    try {
+        const views = await View.findAll({ where: { id_google } });
+        
+        // calcul moyenne avis
+        if (views.length > 0) {
+            const totalNotes = views.reduce((sum, view) => sum + view.note, 0);
+            const averageNote = totalNotes / views.length;
+
+            res.status(200).json({
+                averageNote: averageNote.toFixed(2) // 2 décimales
+            });
+        } else {
+            res.status(404).json({ error: 'Aucun avis trouvé pour cet id_google' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des avis et du calcul de la moyenne' });
+    }
+};
